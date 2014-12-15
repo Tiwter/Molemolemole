91362e83c4089f06f1470052cedd1e5150ab1460
tiwter <tototiw10@hotmail.com>
2014-12-15 11:56:27 +0700

    hit mole and get score

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 import gamelib
 from Hole import Hole
 from Mole import Mole
+GREY = pygame.Color('grey')
 
 class MoleGame(gamelib.SimpleGame):
 	BROWN = pygame.Color('brown')
@@ -11,13 +12,18 @@ class MoleGame(gamelib.SimpleGame):
 	def __init__(self):
 		super(MoleGame, self).__init__('Mole', MoleGame.BROWN)
 		self.mole = Mole()
-		self.time = 1000
+		self.time = 1800
+		self.score = 0
+		self.time2 = 0
 		self.holes = (Hole((150,150)),Hole((280,150)),Hole((410,150)),
 		Hole((150,250)),Hole((280,250)),Hole((410,250)),
 		Hole((150,350)),Hole((280,350)),Hole((410,350)))
 
 	def init(self):
+		global font
 		super(MoleGame, self).init()
+		font = pygame.font.SysFont("monospace", 20)
+		self.render_score()
 
 	def __handle_event(self):
 		for event in pygame.event.get():
@@ -28,7 +34,14 @@ class MoleGame(gamelib.SimpleGame):
 				if(self.is_hit(self.mole)):
 					self.mole_get_hit()
 
+	def render_score(self):
+		#global score_image
+		self.score_image = font.render("Score = %d" % self.score, 0, GREY)
+
 	def render(self, surface):
+		global score_image
+		surface.blit(self.score_image, (10,10))
+		self.render_score()
 		for hole in self.holes:
 			hole.render(surface)
 		self.mole.render(surface)
@@ -36,11 +49,13 @@ class MoleGame(gamelib.SimpleGame):
 	def update(self):
 		self.__handle_event()
 		self.mole.update(self.holes)
-		self.time -= 1
-		#print self.time
+		self.time += 1
+		self.time2 = self.time/60
+		print self.time2
 
 	def mole_get_hit(self):
 		self.mole.reset_mole_t()
+		self.score += 1
 
 	def is_hit(self, mole):
 		mouse_pos = pygame.mouse.get_pos()
